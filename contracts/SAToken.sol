a87cd576fda69d87f5e8e69427cdd882ce7845a8
SubAlgo <kornchaval@gmail.com>
2021-08-13 19:34:52 +0700

    end clip

---
+++ contracts/SAToken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SAToken is ERC20 {
+  /*
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    _mint(msg.sender, 100000 * (10 ** 18));
+  }
+  */
+  constructor() ERC20("SubAlgo Token", "SAT") {
+
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
 import Subalgo from './artifacts/contracts/Subalgo.sol/Subalgo.json'
-//import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import Token from './artifacts/contracts/Token.sol/Token.json'
+
 
 const subAlgoAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-
+const tokenAddress = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"
 function App() {
   const [name, setNameValue] = useState('')
   const [lastname, setLastnameValue] = useState('')
 
+  const [userAccount, setUserAccount] = useState('')
+  const [amount, setAmount] = useState(0)
+
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
+
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transacion = await contract.transfer(userAccount, amount);
+      await transacion.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
   }
 
   async function fetchData() {
@@ -79,6 +105,18 @@ function App() {
             placeholder="Set lastname" 
           />
           <button onClick={setSubalgoLastname}>Set Lastname</button>
+        </div>
+        <br />
+
+        <div>
+          <button onClick={getBalance}>Get Balance</button>
+          <div>
+            <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID"></input>
+            <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+            <p>
+            <button onClick={sendCoins}>Send coins</button>
+            </p>
+          </div>
         </div>
         
         
